e8c9eeec6a532a363061cd25affd7fd88566ebea
cgaffga@gmail.com <cgaffga@gmail.com@39c7e6b3-d093-136e-b744-a7e747afc4b5>
2012-01-24 22:37:40 +0000

    minor update

--- src/net/watermark/Watermark.java
+++ src/net/watermark/Watermark.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2012 by Christoph Gaffga licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and limitations under the
+ * License.
+ */
+
 package net.watermark;
 
 import java.awt.Color;
@@ -15,9 +24,9 @@ import javax.imageio.ImageIO;
 import net.util.Bits;
 
 /**
- * TODO
+ * Implementation of a watermarking also. See https://code.google.com/p/dct-watermark/
  * 
- * @author cgaffga
+ * @author Christoph Gaffga
  */
 public class Watermark {
 
